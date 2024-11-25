702c8bb44bfe7490360d9f58a848a08db7287c39
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-11-25 16:49:35 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -89,13 +89,17 @@ $(document).ready(function(){
 		$('.modal_close_overlay').hide();
 		$('.modal_main_ebook').hide();
 		$('.modal_main_registraion').hide();
+		$('.modal_main_job').hide();
+		$('.modal_main_apply_job').hide();
 		return false;
 	})
 
 	$(document).on('click','.login_show_form, .show_login_modal', function(){
 		$('.modal_close_overlay').show();
 		$('.modal_main_registraion').hide();
-		$('.modal_main_ebook').hide();
+		$('.modal_main_ebook').hide(); 
+		$('.modal_main_job').hide();
+		$('.modal_main_apply_job').hide();
 		$('.modal_main_login').show(); 
 		return false;
 	})
@@ -104,6 +108,8 @@ $(document).ready(function(){
 		$('.modal_close_overlay').show();
 		$('.modal_main_login').hide();
 		$('.modal_main_ebook').hide();
+		$('.modal_main_job').hide();
+		$('.modal_main_apply_job').hide();
 		$('.modal_main_registraion').show();
 		return false;
 	})
@@ -113,7 +119,35 @@ $(document).ready(function(){
 		$('.modal_main_login').hide();
 		$('.modal_main_registraion').hide();
 		$('.modal_main_login').hide();
+		$('.modal_main_job').hide();
+		$('.modal_main_apply_job').hide();
 		$('.modal_main_ebook').show();
+		return false;
+	})
+
+	$(document).on('click','.job_right_btn a', function(){
+		$('.modal_close_overlay').show();
+		$('.modal_main_login').hide();
+		$('.modal_main_registraion').hide();
+		$('.modal_main_login').hide();
+		$('.modal_main_ebook').hide();
+		$('.modal_main_apply_job').hide();
+		$('.modal_main_job').show();
+
+		$('html, body').animate({ scrollTop: 0 }, 'fast'); 
+		return false;
+	})
+
+	$(document).on('click','.apply_confirm', function(){
+		$('.modal_close_overlay').show();
+		$('.modal_main_login').hide();
+		$('.modal_main_registraion').hide();
+		$('.modal_main_login').hide();
+		$('.modal_main_ebook').hide();
+		$('.modal_main_job').hide();
+		$('.modal_main_apply_job').show();
+
+		$('html, body').animate({ scrollTop: 0 }, 'fast'); 
 		return false;
 	})
  
@@ -142,4 +176,96 @@ $(document).ready(function(){
 	
 
 
+});
+
+
+
+
+// File upload get html 
+
+$(document).ready(function() {
+	const fileUploadBox = $('.file-upload-box');
+	const fileList = $('.file-list');
+	const fileInput = $('.file-upload-input');
+
+	// Handle drag and drop events
+	fileUploadBox
+		.on('dragover dragenter', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			$(this).addClass('drag-over');
+		})
+		.on('dragleave dragend drop', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			$(this).removeClass('drag-over');
+		});
+
+	// Handle file selection
+	fileInput.on('change', function(e) {
+		const files = e.target.files;
+		handleFiles(files);
+	});
+
+	// Handle dropped files
+	fileUploadBox.on('drop', function(e) {
+		const files = e.originalEvent.dataTransfer.files;
+		handleFiles(files);
+	});
+
+	function handleFiles(files) {
+		Array.from(files).forEach(file => {
+			// Create progress bar element
+			const progressBar = $('<div class="upload-progress"></div>');
+			
+			const fileItem = $(`
+				<div class="file-item">
+					<i class="fas fa-file file-icon"></i>
+					<span class="file-name" title="${file.name}">${file.name}</span>
+					<i class="fas fa-times remove-file"></i>
+					${progressBar.prop('outerHTML')}
+				</div>
+			`);
+
+			fileList.append(fileItem);
+
+			// Remove progress bar after animation
+			setTimeout(() => {
+				fileItem.find('.upload-progress').remove();
+			}, 1000);
+
+			// Handle file removal
+			fileItem.find('.remove-file').on('click', function(e) {
+				e.stopPropagation();
+				fileItem.fadeOut(300, function() {
+					$(this).remove();
+				});
+			});
+
+			// Get appropriate FontAwesome icon based on file type
+			const fileIcon = fileItem.find('.file-icon');
+			const fileExtension = file.name.split('.').pop().toLowerCase();
+			
+			const iconMap = {
+				'pdf': 'fa-file-pdf',
+				'doc': 'fa-file-word',
+				'docx': 'fa-file-word',
+				'xls': 'fa-file-excel',
+				'xlsx': 'fa-file-excel',
+				'ppt': 'fa-file-powerpoint',
+				'pptx': 'fa-file-powerpoint',
+				'jpg': 'fa-file-image',
+				'jpeg': 'fa-file-image',
+				'png': 'fa-file-image',
+				'gif': 'fa-file-image',
+				'zip': 'fa-file-archive',
+				'rar': 'fa-file-archive',
+				'txt': 'fa-file-alt'
+			};
+
+			if (iconMap[fileExtension]) {
+				fileIcon.removeClass('fa-file').addClass(iconMap[fileExtension]);
+			}
+		});
+	}
 });
